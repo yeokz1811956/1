95ebca3397bbc0598b49b6e701f1685c06576108
B1 <B1@B150>
2019-11-05 08:35:35 +0800

    1

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println"faku";
+        System.out.println("faku");
     }
     
 }
